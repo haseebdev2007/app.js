c3a0511715d1fb8f94f4f903a9a74083514c6740
Abdul Haseeb <abdulhaseeb.std07@email.com>
2025-04-16 02:22:51 -0700

    update

---
+++ app.js
@@ -0,0 +1,52 @@
+// chapter 1
+alert("first name");
+alert("last name");
+alert("email");
+alert("phone number");
+alert("password");
+alert("i am learning javascript");
+alert("jawan pakistan is best ");
+
+
+// chapter 2
+
+
+// 01: 
+let userName = "muhammadHaseeb"
+
+// 02;
+let schoolName;
+schoolName = 'the gateway public school';
+
+// 03:
+let city = "karachi";
+alert(city);
+
+// 04:
+var name = "razaq";
+name = 'abdulRazaq';
+
+
+
+// chapter 3
+
+
+// 01:
+let cans;
+// 02: 
+cans = 144;
+// 03:
+let colaNext = 100;
+let fizzUp = 110;
+console.log(colaNext+fizzUp);
+// 04:
+var insolin = 5000;
+var sugicalBlade = 40;
+var totalPrice = insolin+sugicalBlade;
+
+console.log(totalPrice);
+// 05:
+let number = 30;
+let adding = (number+5)
+
+console.log(adding);
