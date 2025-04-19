f82a65d7a0cd1536f54c0f607dbb9d9e5f089fbe
Abdul Haseeb <abdulhaseeb.std07@email.com>
2025-04-19 00:55:40 -0700

    js2 added

---
+++ js2/app.js
@@ -0,0 +1,75 @@
+//chapter4
+// 01
+let name = 'muhammadHaseeb', age = 18, qualification = 'matric';
+//02
+//legal variable
+let _hasseeb = 'awais';
+let $fatherName = 'iqbal'
+let user_age = 20;
+let eanrningUSA$ = 20;
+let caste = 'tanoly'
+
+//illegal vriable
+// let #FirstName = 'haseeb';
+// let 10fatherName = 'ashraf';
+// let @email = 'abdulhaseeb.std07@gmail.com';
+// let phone#number = 03121051767;
+// let ^Maritalstatus = 'singal';
+
+
+const myElement = document.getElementById("demo");
+myElement.style.color = "red";
+
+
+const myPara = document.getElementById("para");
+myPara.style.color = "green";
+
+
+
+//chapter5
+var number1 = 20;
+var number2 = 60;
+
+var plus = number1 + number2;
+
+console.log(plus);
+
+
+
+
+// a. Declare a variable
+var num;
+
+// b. Show the value of variable in your browser
+document.write("Value after variable declaration is: " + num + "<br>");
+
+// c. Initialize the variable with some number
+num = 5;
+
+// d. Show the initial value
+document.write("Initial value: " + num + "<br>");
+
+// e. Increment the variable
+num++;
+
+// f. Show value after increment
+document.write("Value after increment is: " + num + "<br>");
+
+// g. Add 7 to the variable
+num += 7;
+
+// h. Show value after addition
+document.write("Value after addition is: " + num + "<br>");
+
+// i. Decrement the variable
+num--;
+
+// j. Show value after decrement
+document.write("Value after decrement is: " + num + "<br>");
+
+// k. Show the remainder after dividing the variable's value by 3
+var remainder = num % 3;
+
+// l. Output the remainder
+document.write("The remainder is: " + remainder);
+
