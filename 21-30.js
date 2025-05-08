54271f5884b2ac61d643b3d18f0de15beedc19d0
haseebdev2007 <abdulhaseeb.std07@gmail.com>
2025-05-08 16:21:37 +0500

    Add files via upload

---
+++ 21-30.js
@@ -0,0 +1,37 @@
+//no 1
+var allLower = userInput.toLowerCase();
+//no 2
+x = x.toLowerCase();
+//no 3
+y = y.toUpperCase();
+//no 4
+var newVar = someString.toLowerCase();
+//no 5
+var newVar = array[0].toLowerCase();
+//no 6
+alert(variable.toUpperCase());
+//no 7
+var cityName = "kaRacHi";
+var formattedCity = cityName.charAt(0).toUpperCase() + cityName.slice(1).toLowerCase();
+//no 8
+var sameWords = "captain";
+var segment = sameWords.slice(1, 3);
+//no 9
+var strLength = someString.length;
+//no 10
+var animal = "elephant";
+var seg = animal.slice(2, 6);
+//no 11
+var length = str.length;
+//no 12
+var length = str.length;
+var result = str.slice(1, length - 3);
+//no 13
+var text = "To be or not to be.";
+var indx = text.indexOf("be"); // Returns 3
+//no 14
+var text = "To be or not to be.";
+var indx = text.lastIndexOf("be"); // Returns 16
+//no 15
+var indx = text.lastIndexOf("go");
+
