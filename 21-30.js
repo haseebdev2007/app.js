30ffd68d8ff44ddcf56fbe4bfcd14070cce53061
haseebdev2007 <abdulhaseeb.std07@gmail.com>
2025-05-16 15:32:02 +0500

    Add files via upload

--- 21-30.js
+++ 21-30.js
@@ -28,10 +28,40 @@ var length = str.length;
 var result = str.slice(1, length - 3);
 //no 13
 var text = "To be or not to be.";
-var indx = text.indexOf("be"); // Returns 3
+var indx = text.indexOf("be"); 
 //no 14
 var text = "To be or not to be.";
-var indx = text.lastIndexOf("be"); // Returns 16
+var indx = text.lastIndexOf("be"); 
 //no 15
 var indx = text.lastIndexOf("go");
+
+
+//rounding number
+//no1
+let rounded = Math.round(4.6);  
+//no2
+let myNumber = 3.2;
+let myRoundNum = Math.ceil(myNumber); 
+//no3
+let myNumber = 7.9;
+let myRoundNum = Math.floor(myNumber);  
+//no4
+let num = 6.4;
+let result = Math.round(num);
+//no5
+let customRounded = Math.floor(0.5); 
+
+
+// (Random Numbers)
+//no1
+let randomInRange = Math.floor(Math.random() * 50) + 1;
+//no2
+let randomNum = Math.random();
+//no3
+let dice = Math.floor(Math.random() * 6) + 1;
+//no4
+let toss = Math.random() < 0.5 ? "Heads" : "Tails";
+
+
+
 
